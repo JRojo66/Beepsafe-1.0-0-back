bc7b8a2eadda62604f7ec4161715c2b0f2d58baf
jrojo66 <javier.rojo66@gmail.com>
2025-04-29 14:31:27 -0300

    al 29.Abr.25c

--- src/routes/configRouter.js
+++ src/routes/configRouter.js
@@ -9,6 +9,6 @@ configRouter.get("/", (req, res) => {
   res.json({
     maxLoginAttempts: parseInt(process.env.LOGIN_MAX_ATTEMPTS) || 3,
     blockTimeMinutes: parseInt(process.env.LOGIN_BLOCK_TIME_MINUTES) || 60
-    //rootURL: parseInt
+    
   });
 });
